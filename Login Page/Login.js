8af3f57ee5c91010edeb1c0535e7466e67ffc946
Vishnu Anand <vishnuanand2000@gmail.com>
2021-03-30 08:48:33 +0530

    Implemented page splitting based on username

--- Login Page/Login.js
+++ Login Page/Login.js
@@ -4,26 +4,30 @@ function login()
 		var SRN = document.getElementById("SRN").value;
 		var pwd = document.getElementById("pwd1").value;
 		var filter = /([a-zA-Z0-9_\.\-])$/;
-		if(SRN=='')
+		if(SRN=='') //Checking blank form
 		{
-			alert("please enter user name.");
+			alert("Enter valid Student Number or Faculty Number");
 		}
-		else if(pwd=='')
+		else if(pwd=='') //Checking blank form
 		{
-        	alert("enter the password");
+        	alert("Enter the password");
 		}
 		else if(!filter.test(SRN))
 		{
-			alert("Enter valid email id.");
+			alert("Enter valid email id");
+		}
+		else if(SRN.length != 13) 
+		{
+			alert("Enter valid Student Number or Faculty Number")
 		}
 		else if(pwd.length < 3 || pwd.length > 10)
 		{
-			alert("Password min and max length is 6.");
+			alert("Password min and max length is 6");
 		}
 		else
 		{
-            //Redirecting to other page or webste code or you can set your own html page.
-            window.location = "Blank.html";
+            //Checking username
+            CheckUsername();
         }
 	}
 	//Reset Inputfield code.
@@ -32,3 +36,21 @@ function login()
 		document.getElementById("SRN").value="";
 		document.getElementById("pwd1").value="";
 	}	
+
+	//Username Checker
+	function CheckUsername()
+	{
+		var uname = document.getElementById("SRN");
+		if(uname.value[0] == 'F' && uname.value[1] == 'R' && uname.value[2] == 'N'){
+			//Also run verification with database
+			window.location = "Faculty.html";
+		}
+		else if (uname.value[0] == 'P' && uname.value[1] == 'E' && uname.value[2] == 'S'){
+			//Also run verification with database
+			window.location = "Student.html";
+		}
+		else{
+			//If database check fails
+			alert("Please enter valid details");
+		}
+	}
